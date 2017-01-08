341c0e769ef8929df8af5a87c5a8d12d362d4651
Wuilliam Lacruz <wuilliam321@gmail.com>
2017-01-08 02:23:01 -0400

    Fix

--- wsgi/youdown/download/urls.py
+++ wsgi/youdown/download/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'download'
+# app_name = 'download'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^info/$', views.info, name='info'),
